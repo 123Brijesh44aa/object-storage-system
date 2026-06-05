35b042927cd6aca532c631cfa97bd12e16e585aa
123Brijesh44aa <oddsworld0121@gmail.com>
2026-06-05 16:32:54 +0530

    Initial commit : add auth-service

---
+++ auth-service/src/test/java/com/brijesh/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brijesh.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
